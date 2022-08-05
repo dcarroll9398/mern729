249c9f47772ff8abec4250b7594f6f8e56911bb4
dcarroll9398 <dcarroll9398@yahoo.com>
2022-08-04 23:47:00 -0500

    Video-21

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Wayne',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Gretz',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: 'Nike Slim shirt',
